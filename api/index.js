d4aac9010be7865876b69aac30895b9f659da6f1
jeannie <jeanniezheng123@gmail.com>
2023-08-02 11:06:39 -0500

    Add functionality to upload photos

--- api/index.js
+++ api/index.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const imageDownloader = require('image-downloader');
+const multer = require('multer');
+const fs = require('fs');
 
 const User = require('./models/User.js');
 require('dotenv').config()
@@ -14,6 +17,7 @@ const jwtSecret = 'randomstringfornow'
 
 app.use(express.json());
 app.use(cookieParser());
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
 app.use(cors({
     credentials: true,
@@ -85,5 +89,31 @@ app.get('/profile', (req, res) => {
     }
 });
 
+app.post('/upload-by-link', async (req, res) => {
+    const { link } = req.body;
+    const newName = "photo" + Date.now() + '.jpg';
+    console.log("dirrrrname" + { __dirname })
+    await imageDownloader.image({
+        url: link,
+        dest: __dirname + '/uploads/' + newName,
+    });
+    res.json(newName)
 
+})
+
+const photosMiddleware = multer({ dest: 'uploads/' });
+
+app.post('/upload', photosMiddleware.array('photos', 100), (req, res) => {
+    const uploadedFiles = [];
+    for (let i = 0; i < req.files.length; i++) {
+        const { path, originalname } = req.files[i];
+        const parts = originalname.split('.');
+        const ext = parts[parts.length - 1];
+        const newPath = path + '.' + ext;
+        fs.renameSync(path, newPath)
+        uploadedFiles.push(newPath.replace('uploads/', ""))
+        console.log(path, " ", originalname)
+    }
+    res.json(uploadedFiles);
+})
 app.listen(4000);
