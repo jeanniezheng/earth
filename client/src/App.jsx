d43b45369250d615c9531501260bd5a05d85bc99
jeannie <jeanniezheng123@gmail.com>
2023-08-07 15:54:18 -0500

    Added fixed layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,19 +24,17 @@ function App() {
     <UserContextProvider>
 
       <Routes>
-        <Route path={"/"} element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<IndexPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/account" element={<ProfilePage />} />
-          <Route path="/account/places" element={<AccountPage />} />
+          <Route path="/account/places" element={<PlacesPage />} />
           <Route path="/account/places/new" element={<PlacesFormPage />} />
-
-
-
+          <Route path="/account/places/:id" element={<PlacesFormPage />} />
         </Route>
       </Routes>
-    </UserContextProvider>
+    </UserContextProvider >
   );
 }
 
